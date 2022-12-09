10a076dae01d1105ff293d23b8706ae0c4c57e5e
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-12-09 11:39:55 +0200

    done

---
+++ main.ts
@@ -0,0 +1,37 @@
+
+/**
+ * START: Follow the instructions below.
+ */
+
+// Add type annotations to each of these variables.
+
+let country: string = "Italy";
+
+let year: undefined = undefined;
+
+let averageTemperature: number = 23.6;
+
+let visited: boolean = true;
+
+let currency: null = null;
+
+// Change the type annotations on these variables so they are correct.
+
+let population: number = 47_450_795;
+
+let isSummer: boolean = false;
+
+let languages: string = "Spanish, English";
+
+// ----
+
+export {
+  country,
+  year,
+  averageTemperature,
+  visited,
+  currency,
+  population,
+  isSummer,
+  languages,
+};
