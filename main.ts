e06427c8ae15d04af85135b42ef81b22be488b34
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-12-13 01:15:38 +0200

    done

---
+++ main.ts
@@ -0,0 +1,44 @@
+
+/**
+ * START: Follow the instructions below.
+ */
+
+// Make this identity function generic.
+// This should fix the type errors on Line 13 and 15.
+// Hint: The type for the `value` function parameter should use a type variable.
+
+function identity<Type>(value: Type) {
+    return value;
+}
+
+let value1 = identity<string>("France");
+
+let value2 = identity<number>(67_413_000);
+
+// ----
+
+async function fetchData<Type>(url: string) {
+    const response = await fetch(url);
+    const responseBody = await response.json();
+
+    return responseBody as Type;
+}
+
+interface User {
+    name: string;
+}
+
+// Pass the `User` type as a type argument to the generic `fetchData()` function.
+// This should fix the type error on Line 35.
+
+let user = await fetchData<User>("http://api.com/user/1");
+// I didn't write this 'await' it was already here. which is wrong because you can't have an await
+// outside of 'async function'
+    
+
+
+console.log(user.name);
+
+// ----
+
+export {};
